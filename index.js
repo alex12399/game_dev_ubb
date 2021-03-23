a361a85e0733f3be2a77edaacf6ce74eb7ae199c
alex cireasa <alex.cireasa.810@gmail.com>
2021-03-23 16:07:36 +0200

    set up linter + prettier

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const app = express();
 const server = http.createServer(app);
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/client/index.html');
-})
+    res.sendFile(`${__dirname}/client/index.html`);
+});
 
-app.use('/client', express.static(__dirname + '/client'));
+app.use('/client', express.static(`${__dirname}/client`));
 
-server.listen(2000)
+server.listen(2000);
+console.log('Server started!');
