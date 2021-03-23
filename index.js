93ff8b0b4114fcc73d783c23b970ebd7d8edfe26
alexcireasalor <acireasa@laingorourke.com>
2021-03-23 14:48:09 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import http from 'http';
+
+const app = express();
+const server = http.createServer(app);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/client/index.html');
+})
+
+app.use('/client', express.static(__dirname + '/client'));
+
+server.listen(2000)
